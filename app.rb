e62b7641c007883495dbbb32bbbf26ac5d4f5238
jackymiranda <jackelyn.miranda@chicagobooth.edu>
2024-10-22 20:22:34 +0000

    tareaterminada

--- app.rb
+++ app.rb
@@ -2,8 +2,54 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:homepage)
+end
+
+get ("/rock") do
+  
+  moves=["rock","paper","scissors"]
+  @comp_moves=moves.sample
+
+  if @comp_moves=="rock"
+    @outcome = "tied"
+  elsif @comp_moves=="paper"
+    @outcome= "lost"
+  else 
+    @outcome="won"
+  end
+
+erb(:zebra)
+
+end
+
+get ("/paper") do
+    
+  moves=["rock","paper","scissors"]
+  @comp_moves=moves.sample
+
+  if @comp_moves=="paper"
+    @outcome = "tied"
+  elsif @comp_moves=="scissors"
+    @outcome= "lost"
+  else 
+    @outcome="won"
+  end
+
+  erb(:giraffe)
+end
+
+get ("/scissors") do
+    
+  moves=["rock","paper","scissors"]
+  @comp_moves=moves.sample
+
+  if @comp_moves=="scissors"
+    @outcome = "tied"
+  elsif @comp_moves=="rock"
+    @outcome= "lost"
+  else 
+    @outcome="won"
+  end
+
+  erb(:elephant)
 end
